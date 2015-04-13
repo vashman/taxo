e72795eac4c45d003ab204ee31ce20262763dae4
Sunny S <3030712@gmail.com>
2015-04-13 00:52:45 -0700

    added degree member to realtionship.

--- include/relation.hpp
+++ include/relation.hpp
@@ -3,19 +3,21 @@
 #ifndef TAXO_RELATION_HPP
 #define TAXO_RELATION_HPP
 
+#include "tag.hpp"
+
 namespace taxo {
 /**/
-template <typename charT, typename traits = std::char_traits<charT>, typename allocator = std::allocator<charT> >
-class basic_relation : public basic_catagory<charT,traits,allocator> {
+template <
+  typename T
+, typename charT
+, typename traits = std::char_traits<charT>
+, typename allocator = std::allocator<charT>
+>
+class basic_relation : public basic_tag<charT,traits,allocator> {
 public:
-  typedef int realtionship_type;
-  static int const composition = 
-  static int const assocation = 
-
-private:
-  int differnece, likeness;
-  int relationship;
+  T degree;
 };
 
 } /* taxo */
+#include "bits/realtion.tcc"
 #endif
